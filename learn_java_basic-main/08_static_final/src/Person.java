7619639ca207dedc3dcb4b31a85d60222905ceda
y-masuda1126 <y-masuda@clinks.jp>
2024-11-21 02:31:33 +0900

    Add files via upload

---
+++ learn_java_basic-main/08_static_final/src/Person.java
@@ -0,0 +1,4 @@
+class Person {
+    static String name; // 名前
+    static int age; // 年齢
+}
---
+++ learn_java_basic-main/08_static_final/src/StaticCalculator.java
@@ -0,0 +1,10 @@
+class StaticCalculator {
+    static int add(int x, int y) { //【1】
+        int result = x + y;
+        return result;
+    }
+    static int subtract(int x, int y) { //【2】
+        int result = x - y;
+        return result;
+    }
+}
